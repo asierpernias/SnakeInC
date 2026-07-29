745db38f1ff18f6467f69f57d7aebdb1b102492b
APSwinner <ague_soria@yahoo.es>
2026-07-29 11:53:43 +0200

    Snake and using getch to detect keys and move it

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <ncurses.h>
 #include <stdlib.h>
-
+struct Punto {
+    int x;
+    int y;
+};
+  
 int main() {
     initscr();
     noecho();
@@ -10,8 +14,14 @@ int main() {
     int alive = 1;
     int ANCHO = 128;
     int ALTO = 32;
-    int x = 0;
-    int y = 0;
+
+    struct Punto serpiente[256];
+    int longitud = 1;
+    int dx = 0;
+    int dy = 0;
+    serpiente[0].x = ANCHO / 2;
+    serpiente[0].y = ALTO / 2;
+
     while (alive != 0) {
         clear();
 
@@ -35,6 +45,38 @@ int main() {
                     }
                 }
             }
+        }
+
+        int tecla = getch();
+
+        switch (tecla) {
+            case KEY_UP:
+                dy = -1;
+                break;
+            
+            case KEY_DOWN:
+                dy= 1;
+                break;
+            
+            case KEY_LEFT:
+                dx= -1;
+                break;
+            
+            case KEY_RIGHT:
+                dx = 1;
+                break
+            
+
+        for (int i = longitud; i > 0 ; i--) {
+            serpiente[i].y = serpiente[i - 1].y;
+            serpiente[i].x = serpiente[i - 1].x;
+        }
+
+        serpiente[0].x = serpiente[0].x + dx;
+        serpiente[0].y = serpiente[0].y + dy;
+
+        for (int i = 0; i < longitud; i++) {
+            mvprintw(serpiente[i].y, serpiente[i].x, "0");
         }
     }
     endwin();
